b36bd9f2f3d615c3b37ef771e8b92b7fb5f34d99
delphython <delphython@gmail.com>
2022-01-27 10:08:47 +0300

    Add redirect from admin order page after saving

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 from django.shortcuts import reverse
 from django.templatetags.static import static
+from django.utils.encoding import iri_to_uri
 from django.utils.html import format_html
+from django.utils.http import url_has_allowed_host_and_scheme
 
 from .models import Product
 from .models import ProductCategory
@@ -120,6 +123,16 @@ class ProductAdmin(admin.ModelAdmin):
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     inlines = [OrderProductsInline]
+
+    def response_change(self, request, obj):
+        res = super(OrderAdmin, self).response_change(request, obj)
+        if "next" in request.GET:
+            if url_has_allowed_host_and_scheme(request.GET["next"], None):
+                return HttpResponseRedirect(iri_to_uri(request.GET["next"]))
+            else:
+                raise
+        else:
+            return res
 
 
 @admin.register(OrderProducts)
