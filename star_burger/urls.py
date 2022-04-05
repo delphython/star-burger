9b937d2cd9966e276bf5f1ddef65875693ebd754
delphython <delphython@gmail.com>
2022-04-05 11:32:25 +0300

    Use url instead of path

--- star_burger/urls.py
+++ star_burger/urls.py
@@ -30,10 +30,10 @@ urlpatterns = [
     path("api/", include("foodcartapp.urls")),
     path("manager/", include("restaurateur.urls")),
     path("api-auth/", include("rest_framework.urls")),
-    path(
+    url(
         r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}
     ),
-    path(
+    url(
         r"^static/(?P<path>.*)$",
         serve,
         {"document_root": settings.STATIC_ROOT},
