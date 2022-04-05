dc5cad327960dc6d4c2dbb30f5da54c1875c2100
delphython <delphython@gmail.com>
2022-04-05 11:14:20 +0300

    Change allowed_hosts getting from env

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -15,7 +15,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 SECRET_KEY = env("SECRET_KEY", "etirgvonenrfnoerngorenogneongg334g")
 DEBUG = env.bool("DEBUG", True)
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", ["127.0.0.1", "localhost"])
+ALLOWED_HOSTS = env("ALLOWED_HOSTS", "127.0.0.1 localhost").split()
 
 INSTALLED_APPS = [
     "foodcartapp.apps.FoodcartappConfig",
