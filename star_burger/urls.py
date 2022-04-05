1391a1474b74251717d0d5dbe3a28261d411ba49
delphython <delphython@gmail.com>
2022-04-05 11:33:52 +0300

    Remove unnecessary import

--- star_burger/urls.py
+++ star_burger/urls.py
@@ -18,7 +18,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import render
-from django.views.static import serve
 
 from . import settings
 
