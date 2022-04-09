27d12b779f096e4e11c6a2f640f48fc02c283cd8
delphython <delphython@gmail.com>
2022-04-09 10:09:36 +0300

    Move Excluding404 to first item in middleware

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "rollbar.contrib.django.middleware.RollbarNotifierMiddlewareOnly404",
+    "rollbar.contrib.django.middleware.RollbarNotifierMiddlewareExcluding404",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -43,7 +43,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
-    "rollbar.contrib.django.middleware.RollbarNotifierMiddlewareExcluding404",
 ]
 
 ROOT_URLCONF = "star_burger.urls"
